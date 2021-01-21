fb5df44f0dbfb9301c2b332e27db9d69fd9c0284
Christian Zarmakoupis <chrzarma@gmail.com>
2021-01-21 17:23:46 +0200

    Create Formatter class and add helper method to simplify our code

--- Formatter/Formatter.swift
+++ Formatter/Formatter.swift
@@ -7,15 +7,18 @@
 
 import Foundation
 
-func format(_ value: Decimal, locale: Locale) -> String {
-    let nf = NumberFormatter()
-    nf.maximumFractionDigits = 2
-    nf.minimumFractionDigits = 2
-    nf.locale = locale
-    
-    if value < 1 {
-        nf.maximumSignificantDigits = 2
-    }
-    
-    return nf.string(from: value as NSNumber) ?? ""
-}
+public final class Formatter {
+    public static func format(_ value: Decimal, locale: Locale) -> String {
+        let nf = NumberFormatter()
+        nf.maximumFractionDigits = 2
+        nf.minimumFractionDigits = 2
+        nf.locale = locale
+
+        if value < 1 {
+            nf.maximumSignificantDigits = 2
+        }
+
+        return nf.string(from: value as NSNumber) ?? ""
+     }
+ }
+
--- FormatterTests/FormatterTests.swift
+++ FormatterTests/FormatterTests.swift
@@ -10,24 +10,30 @@ import XCTest
 
 class FormatterTests: XCTestCase {
     func test_twoFractionDigits() {
-        XCTAssertEqual(format(1.23, locale: Locale(identifier: "en_US")), "1.23")
-        XCTAssertEqual(format(4.56, locale: Locale(identifier: "en_US")), "4.56")
-        XCTAssertEqual(format(7.89, locale: Locale(identifier: "en_US")), "7.89")
-        XCTAssertEqual(format(0.12, locale: Locale(identifier: "en_US")), "0.12")
+        XCTAssertEqual(format(1.23), "1.23")
+        XCTAssertEqual(format(4.56), "4.56")
+        XCTAssertEqual(format(7.89), "7.89")
+        XCTAssertEqual(format(0.12), "0.12")
     }
     
     func test_roundsValuesToTwoFractionDigits() {
-        XCTAssertEqual(format(1.234, locale: Locale(identifier: "en_US")), "1.23")
-        XCTAssertEqual(format(1.235, locale: Locale(identifier: "en_US")), "1.24")
-        XCTAssertEqual(format(1.236, locale: Locale(identifier: "en_US")), "1.24")
-        XCTAssertEqual(format(1.2300, locale: Locale(identifier: "en_US")), "1.23")
-        XCTAssertEqual(format(1.00023, locale: Locale(identifier: "en_US")), "1.00")
+        XCTAssertEqual(format(1.234), "1.23")
+        XCTAssertEqual(format(1.235), "1.24")
+        XCTAssertEqual(format(1.236), "1.24")
+        XCTAssertEqual(format(1.2300), "1.23")
+        XCTAssertEqual(format(1.00023), "1.00")
     }
     
     func test_roundsValuesToTwoSignificantDigitsWhenLessThanOne() {
-        XCTAssertEqual(format(0.000032, locale: Locale(identifier: "en_US")), "0.000032")
-        XCTAssertEqual(format(0.0000323343, locale: Locale(identifier: "en_US")), "0.000032")
-        XCTAssertEqual(format(0.0000328103, locale: Locale(identifier: "en_US")), "0.000033")
-        XCTAssertEqual(format(0.0000325103, locale: Locale(identifier: "en_US")), "0.000033")
+        XCTAssertEqual(format(0.000032), "0.000032")
+        XCTAssertEqual(format(0.0000323343), "0.000032")
+        XCTAssertEqual(format(0.0000328103), "0.000033")
+        XCTAssertEqual(format(0.0000325103), "0.000033")
+    }
+    
+    // MARK: Helpers
+    
+    func format(_ value: Decimal) -> String {
+        Formatter.format(value, locale: Locale(identifier: "en_US"))
     }
 }
