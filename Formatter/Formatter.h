021753cedd37b74c284931e4e62e1c6047de3cc2
Christian Zarmakoupis <chrzarma@gmail.com>
2021-01-21 17:10:30 +0200

    Initial Commit

---
+++ Formatter/Formatter.h
@@ -0,0 +1,18 @@
+//
+//  Formatter.h
+//  Formatter
+//
+//  Created by Christian Zarmakoupis on 21/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Formatter.
+FOUNDATION_EXPORT double FormatterVersionNumber;
+
+//! Project version string for Formatter.
+FOUNDATION_EXPORT const unsigned char FormatterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Formatter/PublicHeader.h>
+
+
