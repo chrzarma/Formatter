fc02f21de3dc758441880ecb7e24d9140441c1de
Christian Zarmakoupis <chrzarma@gmail.com>
2021-01-21 17:15:19 +0200

    Formats decimal values with two fraction digits

--- Formatter/Formatter.swift
+++ Formatter/Formatter.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+func format(_ value: Decimal, locale: Locale) -> String {
+    let nf = NumberFormatter()
+    nf.maximumFractionDigits = 2
+    nf.locale = locale
+    
+    return nf.string(from: value as NSNumber) ?? ""
+}
--- FormatterTests/FormatterTests.swift
+++ FormatterTests/FormatterTests.swift
@@ -9,5 +9,10 @@ import XCTest
 @testable import Formatter
 
 class FormatterTests: XCTestCase {
-
+    func test_twoFractionDigits() {
+        XCTAssertEqual(format(1.23, locale: Locale(identifier: "en_US")), "1.23")
+        XCTAssertEqual(format(4.56, locale: Locale(identifier: "en_US")), "4.56")
+        XCTAssertEqual(format(7.89, locale: Locale(identifier: "en_US")), "7.89")
+        XCTAssertEqual(format(0.12, locale: Locale(identifier: "en_US")), "0.12")
+    }
 }
